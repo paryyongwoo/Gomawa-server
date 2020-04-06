6679dbe32e8acfe9ea0c7376bcca536b9b2c086c
ektto1041 <dhkdwk1041@gmail.com>
2020-04-06 17:50:06 +0900

    닉네임 변경 setNickName 메소드 추가
    key 값으로 레코드를 가져와서 nickName 값만 수정한 후에 update 함

--- src/main/java/com/gomawa/gomawa/controller/MemberController.java
+++ src/main/java/com/gomawa/gomawa/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.gomawa.gomawa.controller;
 
 import com.gomawa.gomawa.entity.Member;
 import com.gomawa.gomawa.service.MemberService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
@@ -34,12 +35,40 @@ public class MemberController {
         // 위 Key 값으로 찾은 Member. 없으면 Null 값이 들어가있음
         Member findedMember = memberService.getMemberByKey(key);
 
-        if(findedMember == null) {
+        if (findedMember == null) {
             // 위 Key 값으로 가입된 Member 가 없다면 ~ 매개변수로 받아온 Member를 DB에 추가하고, 해당 Member 를 반환
             member = memberService.addMember(memberParam);
         } else {
             // 위 Key 값으로 가입된 Member 가 있다면 ~ 새로 DB에 추가하는 것 없이, DB에 있는 Member를 반환
             member = findedMember;
+        }
+
+        return ResponseEntity.ok(member);
+    }
+
+    // PUT
+    @RequestMapping(
+            value = "/api/nickName",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.PUT
+    ) //
+    public ResponseEntity setNickName(@RequestBody Member memberParam) {
+        // 반환되는 Member
+        Member member = null;
+
+        // 매개변수로 받아온 Member의 키값으로 DB에 저장된 Member 를 찾음
+        member = memberService.getMemberByKey(memberParam.getKey());
+
+        if(member == null) {
+            // todo: 매개변수의 키값으로 DB에 저장된 Member를 찾지 못한 경우의 예외 처리
+            System.out.println("DB에서 가져온 Member의 값이 Null입니다.");
+        } else {
+            // 매개변수로 받아온 Member의 닉네임을 DB에서 가져온 Member의 닉네임에 대입
+            member.setNickName(memberParam.getNickName());
+
+            // 닉네임 값만 바뀐 Member를 save한다
+           memberService.addMember(member);
+            System.out.println("@#@#@#@#@#@#@#@#@#@#@#");
         }
 
         return ResponseEntity.ok(member);
